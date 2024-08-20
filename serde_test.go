f8e6a095738c6a503755a4f2f90b8f482ad68b31
Ben Noordhuis <info@bnoordhuis.nl>
2024-08-20 21:56:25 +0200

    dry tests

--- serde_test.go
+++ serde_test.go
@@ -16,76 +16,37 @@ package serde
 
 import (
 	"bytes"
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestReadValue(t *testing.T) {
-	if nil != tryReadValue([]byte{bcVersion, 0, 1}) {
-		panic("nil expected")
-	}
-	if Undefined != tryReadValue([]byte{bcVersion, 0, 2}) {
-		panic("undefined expected")
-	}
-	if false != tryReadValue([]byte{bcVersion, 0, 3}) {
-		panic("false expected")
-	}
-	if true != tryReadValue([]byte{bcVersion, 0, 4}) {
-		panic("true expected")
-	}
-	if int32(42) != tryReadValue([]byte{bcVersion, 0, 5, 84}) {
-		panic("42 expected")
-	}
-	if float64(13.37) != tryReadValue([]byte{bcVersion, 0, 6, 61, 10, 215, 163, 112, 189, 42, 64}) {
-		panic("13.37 expected")
-	}
+	expect(nil, tryReadValue([]byte{bcVersion, 0, 1}))
+	expect(Undefined, tryReadValue([]byte{bcVersion, 0, 2}))
+	expect(false, tryReadValue([]byte{bcVersion, 0, 3}))
+	expect(true, tryReadValue([]byte{bcVersion, 0, 4}))
+	expect(int32(42), tryReadValue([]byte{bcVersion, 0, 5, 84}))
+	expect(float64(13.37), tryReadValue([]byte{bcVersion, 0, 6, 61, 10, 215, 163, 112, 189, 42, 64}))
 	// note: quickjs never produces 0.0 but writes a zero int32 instead
-	if float64(0.0) != tryReadValue([]byte{bcVersion, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0}) {
-		panic("0.0 expected")
-	}
-	if float64(-0.0) != tryReadValue([]byte{bcVersion, 0, 6, 0, 0, 0, 0, 0, 0, 0, 128}) {
-		panic("-0.0 expected")
-	}
-	if "ok" != tryReadValue([]byte{bcVersion, 0, 7, 4, 111, 107}) {
-		panic("\"ok\" expected")
-	}
-	if "😭" != tryReadValue([]byte{bcVersion, 0, 7, 5, 61, 216, 45, 222}) {
-		panic("\"😭\" expected")
-	}
-	if !reflect.DeepEqual([]any{}, tryReadValue([]byte{bcVersion, 0, 9, 0})) {
-		panic("[null] expected")
-	}
-	if !reflect.DeepEqual([]any{nil}, tryReadValue([]byte{bcVersion, 0, 9, 1, 1})) {
-		panic("[null] expected")
-	}
-	if !reflect.DeepEqual([]byte{}, tryReadValue([]byte{bcVersion, 0, 15, 0})) {
-		panic("ArrayBuffer() expected")
-	}
-	if !reflect.DeepEqual([]byte{42}, tryReadValue([]byte{bcVersion, 0, 15, 1, 42})) {
-		panic("ArrayBuffer() expected")
-	}
-	if !reflect.DeepEqual([]byte{42}, tryReadValue([]byte{bcVersion, 0, 14, 2, 1, 0, 15, 1, 42})) {
-		panic("Uint8Array([42]) expected")
-	}
-	if !reflect.DeepEqual(map[string]any{"k": nil}, tryReadValue([]byte{bcVersion, 1, 2, 107, 8, 1, 2, 1})) {
-		panic("{k:null} expected")
-	}
-	if !reflect.DeepEqual(map[string]any{"42": nil}, tryReadValue([]byte{bcVersion, 0, 8, 1, 85, 1})) {
-		panic("{[42]:null} expected")
-	}
-	if !reflect.DeepEqual(map[string]any{"-42": nil}, tryReadValue([]byte{bcVersion, 1, 6, 45, 52, 50, 8, 1, 2, 1})) {
-		panic("{[-42]:null} expected")
-	}
+	expect(float64(0.0), tryReadValue([]byte{bcVersion, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0}))
+	expect(float64(-0.0), tryReadValue([]byte{bcVersion, 0, 6, 0, 0, 0, 0, 0, 0, 0, 128}))
+	expect("ok", tryReadValue([]byte{bcVersion, 0, 7, 4, 111, 107}))
+	expect("😭", tryReadValue([]byte{bcVersion, 0, 7, 5, 61, 216, 45, 222}))
+	expect([]any{}, tryReadValue([]byte{bcVersion, 0, 9, 0}))
+	expect([]any{nil}, tryReadValue([]byte{bcVersion, 0, 9, 1, 1}))
+	expect([]byte{}, tryReadValue([]byte{bcVersion, 0, 15, 0}))
+	expect([]byte{42}, tryReadValue([]byte{bcVersion, 0, 15, 1, 42}))
+	expect([]byte{42}, tryReadValue([]byte{bcVersion, 0, 14, 2, 1, 0, 15, 1, 42}))
+	expect(map[string]any{"k": nil}, tryReadValue([]byte{bcVersion, 1, 2, 107, 8, 1, 2, 1}))
+	expect(map[string]any{"42": nil}, tryReadValue([]byte{bcVersion, 0, 8, 1, 85, 1}))
+	expect(map[string]any{"-42": nil}, tryReadValue([]byte{bcVersion, 1, 6, 45, 52, 50, 8, 1, 2, 1}))
 }
 
 func TestReadObject(t *testing.T) {
 	type empty struct{}
-	if !reflect.DeepEqual(&empty{}, tryReadObject(&empty{}, []byte{bcVersion, 0, 8, 0})) {
-		panic("empty struct expected")
-	}
-	if !reflect.DeepEqual(&empty{}, tryReadObject(&empty{}, []byte{bcVersion, 1, 2, 107, 8, 1, 2, 1})) {
-		panic("empty struct expected")
-	}
+	expect(&empty{}, tryReadObject(&empty{}, []byte{bcVersion, 0, 8, 0}))
+	expect(&empty{}, tryReadObject(&empty{}, []byte{bcVersion, 1, 2, 107, 8, 1, 2, 1}))
 }
 
 func tryReadValue(b []byte) any {
@@ -102,4 +63,10 @@ func tryReadObject(v any, b []byte) any {
 		panic(err)
 	}
 	return v
+}
+
+func expect(expected any, actual any) {
+	if !reflect.DeepEqual(expected, actual) {
+		panic(fmt.Sprintf("expected %v, have %v", expected, actual))
+	}
 }
